87419d3ae2b3d2a0c6e5ac1b4fe96621b35386da
Kristen Peck <kpeck71@gmail.com>
2020-08-05 20:35:30 -0400

    add book button working

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react'
 import * as BooksAPI from './BooksAPI'
 import './App.css'
 import ListBooks from './ListBooks'
+import AddBook from './AddBook'
+import { BrowserRouter, Route, Link } from 'react-router-dom'
 
 class BooksApp extends React.Component {
   state = {
@@ -34,44 +36,42 @@ class BooksApp extends React.Component {
       })
   }
 
+    getResults = (query) => {
+      BooksAPI.search(query)
+          .then((books) => {
+              console.log(books)
+              this.setState((books) => ({
+                  books
+              }))
+          })
+  }
+
   render() {
     return (
       <div className="app">
-        {this.state.showSearchPage ? (
-          <div className="search-books">
-            <div className="search-books-bar">
-              <button className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</button>
-              <div className="search-books-input-wrapper">
-                {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
-                <input type="text" placeholder="Search by title or author"/>
-
-              </div>
-            </div>
-            <div className="search-books-results">
-              <ol className="books-grid"></ol>
-            </div>
-          </div>
-        ) : (
-          <div className="list-books">
-            <div className="list-books-title">
-              <h1>MyReads</h1>
-            </div>
-            <ListBooks books={this.state.books} changeShelf={this.changeShelf}/>
-            
-            <div className="open-search">
-              <button onClick={() => this.setState({ showSearchPage: true })}>Add a book</button>
-            </div>
-          </div>
-        )}
+ 
+        <Route exact path='/' render={() => (
+          <ListBooks 
+          books={this.state.books}
+          changeShelf={this.changeShelf}
+          />
+        )} />
+                    
+        <Route path ='/search' render={({ history }) => (
+          <AddBook 
+            books={this.state.books}
+            getResults={this.getResults}
+            onAddBook={(book) => {
+              this.addBook(book)
+              history.push('/')
+            }} 
+          />
+        )} 
+        />
+      
+      
       </div>
-    )
+      )
   }
 }
 
--- src/ListBooks.js
+++ src/ListBooks.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import BookGrid from './BookGrid'
+import { Link } from 'react-router-dom'
 
 class ListBooks extends React.Component {
 
@@ -19,7 +20,7 @@ class ListBooks extends React.Component {
         //     },
         // ]
 
-
+        
         const { books, changeShelf } = this.props
 
         // let bookshelves = shelfTypes.map(shelf => shelf.heading);
@@ -30,32 +31,43 @@ class ListBooks extends React.Component {
         let haveRead = books.filter(book => book.shelf === "read");
 
         return (
-            <div>
-            <div className="list-books-content">
-                <div className="bookshelf">
-                  <h2 className="bookshelf-title">Currently Reading</h2>
-                  <div className="bookshelf-books">
-                  <BookGrid bookList={currentlyReading} changeShelf={changeShelf}/>        
-                  </div>
+            
+                <div className="list-books">
+                    <div className="list-books-title"><h1>MyReads</h1></div>
+                    <div>
+                        <div className="list-books-content">
+                            <div className="bookshelf">
+                                <h2 className="bookshelf-title">Currently Reading</h2>
+                                <div className="bookshelf-books">
+                                    <BookGrid bookList={currentlyReading} changeShelf={changeShelf}/>        
+                                </div>
+                            </div>
+                        </div>
+                        <div className="list-books-content">
+                            <div className="bookshelf">
+                                <h2 className="bookshelf-title">Want to Read</h2>
+                                <div className="bookshelf-books">
+                                    <BookGrid bookList={toRead} changeShelf={changeShelf}/>        
+                                </div>
+                            </div>
+                        </div>
+                        <div className="list-books-content">
+                            <div className="bookshelf">
+                                <h2 className="bookshelf-title">Read</h2>
+                                <div className="bookshelf-books">
+                                    <BookGrid bookList={haveRead} changeShelf={changeShelf}/>        
+                                </div>
+                            </div>
+                    </div>
                 </div>
+                <Link 
+                to='/search'
+                className='open-search'
+            ><button>Add Book</button></Link>
+                {/* <div className="open-search"><button onClick={() => this.setState({ showSearchPage: true })}>Add a book</button></div> */}
+
             </div>
-            <div className="list-books-content">
-                <div className="bookshelf">
-                <h2 className="bookshelf-title">Want to Read</h2>
-                <div className="bookshelf-books">
-                <BookGrid bookList={toRead} changeShelf={changeShelf}/>        
-                </div>
-                </div>
-            </div>
-            <div className="list-books-content">
-                <div className="bookshelf">
-                <h2 className="bookshelf-title">Read</h2>
-                <div className="bookshelf-books">
-                <BookGrid bookList={haveRead} changeShelf={changeShelf}/>        
-                </div>
-            </div>
-            </div>
-            </div>
+        
         )
         
     }
